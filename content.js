6ba80128e3b48e59112bd0c87f91545d3d291755
zygisau <zygimantasaugunas@gmail.com>
2019-04-27 11:19:03 +0300

    First version of the extension

---
+++ content.js
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action" ) {  
+        // Recolor background
+        document.getElementsByClassName('output-window')[0].style.backgroundColor = 'white';
+
+        // Recolor text background
+        let textArray = document.getElementsByClassName('output-window')[0].getElementsByTagName("pre");
+        for (let textNode in textArray) {
+            // skip loop if the property is from prototype
+            if (!textArray.hasOwnProperty(textNode)) continue;
+
+            textArray[textNode].style.backgroundColor = 'white';
+
+            // Recolor output stream text color
+            if(textArray[textNode].className === "StdOut")
+                textArray[textNode].style.color = 'black';
+        }
+      }
+    });
